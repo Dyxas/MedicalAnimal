78a1e92ca3eafd7e59d529844f9a72554e84a997
Andrey Kuznecov <a.kuznecov@iqdev.digital>
2023-06-04 21:54:38 +0500

    Добавьте файлы проекта.

---
+++ MedicalAnimal/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAnimal
+{
+    internal class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DbConnectionString")
+        {
+
+        }
+
+        public DbSet<Models.ModelCardExample> CardsExample { get; set; }
+    }
+}
---
+++ MedicalAnimal/Models/ModelCardExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedicalAnimal.Models
+{
+    internal class ModelCardExample
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
