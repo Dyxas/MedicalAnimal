8e27d1a1c47d4879cb266074de3928ec8af41149
Andrey Kuznecov <a.kuznecov@iqdev.digital>
2023-06-07 16:55:38 +0500

    Migration

---
+++ MedicalAnimal/Migrations/202306071152373_UpdateContract.cs
@@ -0,0 +1,18 @@
+﻿namespace MedicalAnimal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateContract : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.OrganizationCards", "City", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.OrganizationCards", "City");
+        }
+    }
+}
